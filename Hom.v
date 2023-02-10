523f52e85ddb6813a7e8efb051decf25e229bdb1
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2023-02-09 22:12:16 -0300

    going back to using option to define comm and edge_pres

--- Hom.v
+++ Hom.v
@@ -9,20 +9,6 @@ From KapCake Require Import ContactGraph.
 Local Open Scope fset.
 Local Open Scope fmap.
 
-Lemma eq_fsubset_id (T : choiceType) (A B : {fset T}) :
-  A == B -> A `<=` B.
-Proof. by rewrite eqEfsubset => /andP[H _]. Qed.
-
-Definition change_dom (K V1 V2 : choiceType)
-  (f1 : {fmap K -> V1}) (f2 : {fmap K -> V2})
-  (same_dom : domf f1 == domf f2) (x : domf f1) : domf f2 :=
-  eq_rect (domf f1) id x (domf f2) (eqP same_dom).
-
-Definition fapply (K V1 V2 : choiceType)
-  (f : {fmap K -> V1}) (g : {fmap V1 -> V2})
-  (chainable : codomf f `<=` domf g) (x : domf f) : _ \in codomf g :=
-  in_codomf [`(fsubsetP chainable) _ (in_codomf x)].
-
 Section NS.
 (* Types for nodes and sites *)
 Variables (N S : choiceType).
@@ -48,34 +34,29 @@ Record hom : Type :=
       (* ; comm : [forall s : sites dom, *)
       (*       f_nodes (siteMap dom s) == siteMap cod (f_sites s) *)
       (* ; edge_pres : [forall s : sites dom, forall t : sites dom, *)
-      (*       edges dom s t == *)
-      (*         edges cod (f_sites s) (f_sites t)] *)
-      ; comm : [forall s : sites dom,
-            [`(fsubsetP f_nodes_in_cod) _
-               (fapply (eq_fsubset_id f_nodes_total) s)] ==
-              [`fapply f_sites_in_cod
-                 (@change_dom _ N S (siteMap dom) f_sites
-                    f_sites_total s)]]
-      (* ; edge_pres : [forall s : sites dom, forall t : sites dom, *)
       (*       edges dom (val s) (val t) == *)
       (*         edges cod (f_sites s) (f_sites t)] *)
-      (* ; f_nodes_total : *)
-      (*   [forall n : nodes dom, exists m : nodes cod, *)
-      (*       f_nodes.[? val n] == Some (val m)] *)
-      (* ; f_sites_total : *)
-      (*   [forall s : sites dom, exists t : sites cod, *)
-      (*       f_sites.[? val s] == Some (val t)] *)
-      (* ; comm : *)
-      (*   [forall s : sites dom, *)
-      (*       f_nodes.[? siteMap dom s] == *)
-      (*         obind (fun s' => (siteMap cod).[? s']) *)
-      (*           f_sites.[? val s]] *)
-      (* ; edge_pres : *)
-      (*   [forall s : sites dom, forall t : sites dom, *)
-      (*       edges dom (val s) (val t) == *)
-      (*         if (f_sites.[? val s], f_sites.[? val t]) *)
-      (*              is (Some s', Some t') *)
-      (*         then edges cod s' t' else false] *)
+      ; comm :
+        [forall s : sites dom,
+            f_nodes.[? siteMap dom s] ==
+              if f_sites.[? val s] is Some s'
+              then (siteMap cod).[? s'] else None]
+      ; edge_pres :
+        [forall s : sites dom, forall t : sites dom,
+            edges dom (val s) (val t) ==
+              if (f_sites.[? val s], f_sites.[? val t])
+                   is (Some s', Some t')
+              then edges cod s' t' else false]
     }.
+
+Lemma f_nodes_totalP (h : hom) :
+  [forall n : nodes (dom h), exists m : nodes (cod h),
+      (f_nodes h).[? val n] == Some (val m)].
+Proof using Type. Admitted.
+
+Lemma f_sites_totalP (h : hom) :
+  [forall s : sites (dom h), exists t : sites (cod h),
+      (f_sites h).[? val s] == Some (val t)].
+Proof using Type. Admitted.
 
 End NS.
