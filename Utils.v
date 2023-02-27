23e6d6a2311ddd0bd1cc8146b03224e8df41df25
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2023-02-26 23:42:32 -0300

    removed old files

--- Utils.v
+++
@@ -1,8 +0,0 @@
-Definition is_some {A : Type} (o : option A) : bool :=
-  if o then true else false.
-
-Definition compose {A B C} (g : B -> C) (f : A -> B) :=
-  fun x : A => g (f x).
-#[global] Hint Unfold compose : core.
-Notation "g * f" := (compose g f)
-  (at level 40, left associativity).
